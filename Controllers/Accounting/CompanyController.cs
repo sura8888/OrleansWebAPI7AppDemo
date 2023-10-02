7dd6961b57832c08714df27fd9559aa0173994a7
Kouji Sano <KoujiSano@users.noreply.github.com>
2023-10-02 07:07:43 +0000

    Accounting/Company

---
+++ Controllers/Accounting/CompanyController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using OrleansWebAPI7AppDemo.Models.Accounting;
+using OrleansWebAPI7AppDemo.Models.Demo;
+
+namespace OrleansWebAPI7AppDemo.Controllers.Accounting
+{
+    [ApiController]
+    [Route("Accounting/[controller]/[action]")]
+    public class CompanyController : ControllerBase
+    {
+
+        public CompanyController()
+        {
+        }
+
+        [HttpGet()]
+        [Route("/Accounting/Company")]
+        public Company Index()
+        {
+            var company = new Company();
+            company.Code = "t283162780";
+            company.Name = "デモ株式会社";
+            company.NameKana = "デモカブシキガイシャ";
+            company.代表者姓 = "円簿";
+            company.代表者名 = "太郎";
+            company.代表者フリガナ姓 = "エンボ";
+            company.代表者フリガナ名 = "タロウ";
+            return company;
+        }
+
+    }
+}
---
+++ Models/Accounting/Company.cs
@@ -0,0 +1,24 @@
+﻿using OrleansCodeGen.Orleans.Serialization.Codecs;
+
+namespace OrleansWebAPI7AppDemo.Models.Accounting
+{
+    ///Accounting
+    /// 
+    /// </summary>
+    public class Company
+    {
+        // 会社コード
+        public string Code {get;set; } = String.Empty;
+        // 名前
+        public string Name {get;set;} = String.Empty;
+        // 会社フリガナ
+        public string NameKana {get;set; }  = String.Empty;
+        //
+        public string 代表者 {get;set; }  = String.Empty;
+        //
+        public string 代表者姓 {get;set; }  = String.Empty;
+        public string 代表者名 {get;set; }  = String.Empty;
+        public string 代表者フリガナ姓 {get;set; }  = String.Empty;
+        public string 代表者フリガナ名 {get;set; }  = String.Empty;
+    }
+}
