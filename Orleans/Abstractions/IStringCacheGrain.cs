6d18b3531b3de1fcd0434bd806cdc56f660252b6
佐野浩司 ( Kouji Sano ) <sano@simplise.jp>
2023-11-06 13:40:16 +0900

    remove files

--- Orleans/Abstractions/IStringCacheGrain.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace OrleansWebAPI7AppDemo.Orleans.Abstractions
-{
-    public interface IStringCacheGrain : IGrainWithStringKey
-    {
-        Task SetValue(string value);
-        Task<string> GetValue();
-    }
-}
